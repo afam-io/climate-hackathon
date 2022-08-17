488085287cb3f9a3f7ff59d32c622d37c6a1d63f
AfamOkoh <afamokoh@gmail.com>
2022-08-17 14:38:27 +0100

    feat: score board for different teams carbon spending added

--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
 
+import TeamCard from './TeamCard';
+// import MoreVertIcon from '@mui/icons-material/MoreVert';
+
 export default function Scoreboard() {
-  return <div>Scoreboard</div>;
+  const teamScores = [
+    { name: 'Red', score: 15, color: 'red' },
+    { name: 'Blue', score: 1500, color: 'blue' },
+    { name: 'Green', score: 1000, color: 'green' },
+  ];
+  return teamScores.map((x) => (
+    <TeamCard name={x.name} score={x.score} color={x.color} />
+  ));
+
+  // <TeamCard name={teamScores[0].name} score={teamScores[0].score} />;
 }
