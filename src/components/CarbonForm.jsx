f9d6a4d640251b7a00b82703600f879919e14126
Joe Klakus <joeyk2k@hotmail.com>
2022-08-17 13:42:05 +0100

    formwoking

--- src/components/CarbonForm.jsx
+++ src/components/CarbonForm.jsx
@@ -1,5 +1,61 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Button, MenuItem, TextField } from '@mui/material'
 
 export default function CarbonForm() {
-  return <div>CarbonForm</div>;
+
+  const[formData, setFormData] = useState({})
+
+  const travel = [
+    {
+      value: 'Car',
+      label: 'Car',
+    },
+    {
+      value: 'Bike',
+      label: 'Bike',
+    },
+    {
+      value: 'Walk',
+      label: 'Walk',
+    },
+    {
+      value: 'E-bike',
+      label: 'E-bike',
+    },
+  ];
+
+  const handleInput = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log(formData)
+  }
+
+
+  return <div  style={{ display: 'flex', flexDirection: 'column', height: '100%', width: '80%', margin: 'auto', margin: '20px' }}>
+    <TextField id="outlined-basic" label="Team Name" name="teamName" variant="outlined" onChange={handleInput}/>
+    <TextField
+          id="outlined-select-currency"
+          select
+          name="transportType"
+          label="Travel Type"
+          value={formData.transportType}
+          onChange={handleInput}
+          helperText="Please select your currency"
+        >
+          {travel.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
+        <TextField id="outlined-basic" label="Post Code Start" variant="outlined" onChange={handleInput} name="postCodeStart"/>
+        <TextField id="outlined-basic" name="postCodeEnd" label="Post Code End" variant="outlined" onChange={handleInput}/>
+        <Button onClick={handleClick} variant="contained">Submit</Button>
+  </div>;
 }
